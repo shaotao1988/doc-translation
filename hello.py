33c9612f9c395696ccbf997cf5a2a59d5eaf0277
shaotao1988 <shaotao1988@gmail.com>
2017-03-07 19:56:15 -0500

    Revert "test"
    This reverts commit 8b22c86156de6b3488d8d8104b994520238ea3d5.

--- hello.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-file_name = input("Please input file name:")
-file_object = open(file_name)
-for line in file_object.readlines():
-	print(line.decode('utf8'))
-file_object.close()
-
