ee5f0c33371622c19eee112283f21f3d95b86465
shaotao1988 <shaotao1988@gmail.com>
2017-03-09 00:29:05 -0500

    keep original style of excel
    keep original style of excel

--- hello.py
+++ hello.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python
 
-import xlrd,xlwt,sys
-import codecs
+import xlrd,xlwt,xlutils,sys
+from xlutils.filter import process,XLRDReader,XLWTWriter
+import translate3
+
+def copy2(wb):
+    w = XLWTWriter()
+    process(
+        XLRDReader(wb,'unknown.xls'),
+        w
+        )
+    return w.output[0][1], w.style_list
+
 
 #http://stackoverflow.com/questions/3723793/preserving-styles-using-pythons-xlrd-xlwt-and-xlutils-copy
 def put_value():
-        data = xlrd.open_workbook('test1.xlsx')
-        #table = data.sheet_by_name("sheet1")
-        table = data.sheets()[0]
-        nrows = table.nrows
-        ncols = table.ncols
-        print(nrows)
-        print(ncols)
-        work_book = xlwt.Workbook()
-        work_sheet = work_book.add_sheet('test')
+        origin_work_book = xlrd.open_workbook('test.xls', formatting_info = True)
+        #sheet = origin_work_book.sheet_by_name("sheet1")
+        origin_sheet = origin_work_book.sheets()[0]
+        nrows = origin_sheet.nrows
+        ncols = origin_sheet.ncols
+        print("rows:%d, cols:%d"%(nrows, ncols))
+        work_book, style_list = copy2(origin_work_book)
+        work_sheet = work_book.get_sheet(0)
 
-        translate_col = 1
+        translate_col = 0
         #0 empty, 1 string, 2 number, 3 date, 4 boolean, 5 error
         ctype = 1
+
+        js = translate3.Py4Js()
         
         for rownum in range(0,nrows):
-                #value = table.row(rownum)[translate_col].value.encode('utf8')
-                cell = table.row(rownum)[translate_col]
+                cell = origin_sheet.cell(rownum, translate_col)
                 value = cell.value
-                if value:
+                if len(value) > 0:
+                        xf_index = origin_sheet.cell_xf_index(rownum, translate_col)
                         print(value)
-                        fmt = None
-                        if cell.xf_index != None:
-                                fmt = data.xf_list[cell.xf_index]
-                                work_sheet.write(rownum, 0, value, fmt)
-                        else:
-                                work_sheet.write(rownum, 0, value)
-                #work_sheet.write(rownum, ncols+1, value)
-                #work_sheet.write(rownum, 0, value)
-        work_book.save('t1.xls')
+                        tk = js.getTk(value)
+                        value = translate3.translate(value, tk)
+                        work_sheet.write(rownum, translate_col, value, style_list[xf_index])
+        work_book.save('English.xls')
 
 def main():
         put_value()
